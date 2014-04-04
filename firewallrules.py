ec301a4b78efc8e17b0b670d84b03f325afc6160
laurenyeary <lyeary@colgate.edu>
2014-04-03 21:52:21 -0400

    rules parsed correctly...work on comparing for firewall, then bucket

--- firewallrules.py
+++ firewallrules.py
@@ -12,112 +12,90 @@ from pox.lib.packet import arp
 from pox.lib.addresses import EthAddr,IPAddr,netmask_to_cidr, cidr_to_netmask, parse_cidr
 #from srpy_common import log_info, log_debug, log_warn, SrpyShutdown, SrpyNoPackets, debugger
 
+
 class Rules(object):
     def __init__(self):
-        self.line = 1
-        self.src = 1
-        self.srcPort = 1
-        self.bucket = 1
-        self.type = 1
-        self.yes = 1
-        self.netMask = 1
-        self.dst = 1
-        self.dstPort = 1
-        self.bucket = None 
+        
+        self.line = None
+        self.action = None
+
+        self.type = None  
+        self.src = None
+        self.srcport = None
+        self.dst = None
+        self.dstport = None
+        self.ratelimit = None
+
+        self.netMask = None
+        self.bucket = None
+        
+        self.yes = None
+        
 
 class Firewall(object):
     def __init__(self):
         self.rules = []
         pass
 
-
     #opens txt files and stores all rules in a list 
     def import_rules(self):
         file = open("firewall_rules.txt", "r")
         for line in file:
-            #print "line:    ", line
-            #print len(line)
-            if len(line) != 1:                  #this is to get rid of the empty lines...need to come up with something more clever
+            if len(line) != 1:                  #this is to get rid of the empty lines
                 words = []
                 words = line.split()
-                print "words[0]:    ", words[0]
                 if words[0] == "permit" or words[0] == "deny":          #make sure it is not a comment
-                    print "poop"
                     ruleObject = Rules()
                     ruleObject.line = line
+                    for idx in range(len(words)):
+                        if words[idx] == "permit":
+                            ruleObject.action = "permit"
+                        if words[idx] == "deny":
+                            ruleObject.action = "deny"
+                        if words[idx] == "ip":
+                            ruleObject.type = "ip"
+                        if words[idx] == "tcp":
+                            ruleObject.type = "tcp"
+                        if words[idx] == "udp":
+                            ruleObject.type = "udp"
+                        if words[idx] == "icmp":
+                            ruleObject.type = "icmp"
+                        if words[idx] == "src":
+                            ruleObject.src = words[idx+1]
+                            if "/" in ruleObject.src:               #meaning its a net addr-we need the netmask
+                                temp = parse_cidr(ruleObject.src)
+                                srcnetMask = cidr_to_netmask(temp[1])
+                                ruleObject.netMask = srcnetMask
+                        if words[idx] == "srcport":
+                            ruleObject.srcport = words[idx+1]
+                        if words[idx] == "dst":
+                            ruleObject.dst = words[idx+1]
+                        if words[idx] == "dstport":
+                            ruleObject.dstport = words[idx+1]
+                        if words[idx] == "ratelimit":
+                            ruleObject.ratelimit = words[idx+1]
+
+                    print #we can delete this once its all working
+                    print "************RULE OBJECT****************"  
+                    print ruleObject.line
+                    print ruleObject.action
+                    print ruleObject.type
+                    print ruleObject.src                    
+                    print ruleObject.srcport
+                    print ruleObject.dst
+                    print ruleObject.dstport
+                    print ruleObject.netMask
+                    print ruleObject.ratelimit
+                    print
+                   
+                    self.rules.append(ruleObject)
+
                     #ruleObject.src = line[4]
-                    self.rules.append(ruleObject) #append object instead of line
-
-        print "itterated through"
-        #I'm hard coding this because I don't know what else to do
-        ruleObject = self.rules[0]
-        line = ruleObject.line
-        print "line:   ", line
-        words = []
-        words = line.split()
-
-        #create netmask
-        print "line[4]:    ", words[3]
-        print type(words[3])
-        temp = parse_cidr(words[3])
-        print "srcNetwork:   ", temp
-        srcnetMask = cidr_to_netmask(temp[1])
-        print "netMask:   ", srcnetMask
-        ruleObject.netMask = srcnetMask
-        ruleObject.src = temp[0]
-        ruleObject.type = words[1]
-        ruleObject.yes = words[0]
-        print "finished importing"
-
-
-        #MORE HARDCODING
-        ruleObject = self.rules[1]
-        line = ruleObject.line
-        print "line:   ", line
-        words = []
-        words = line.split()
-
-        #create netmask
-        print "line[4]:    ", words[3]
-        print type(words[3])
-        ruleObject.src = temp[0]
-        ruleObject.type = words[1]
-        ruleObject.yes = words[0]
-        ruleObject.srcPort = words[5]
-        print "finished importing"
-
-
-        #THIRD RULE HARDCODING
-        ruleObject = self.rules[2]
-        line = ruleObject.line
-        print "line:   ", line
-        words = []
-        words = line.split()
-
-        #create netmask
-        print "line[4]:    ", words[3]
-        print type(words[3])
-        ruleObject.src = temp[0]
-        ruleObject.type = words[1]
-        ruleObject.yes = words[0]
-        ruleObject.dstPort = words[9]
-        ruleObject.dst = words[7]
-        print "ruleObject.dst:    ", ruleObject.dst
-        print "finished importing"
-
-
+                    self.rules.append(ruleObject) #append object instead of list
 
     #drop everything from a network that we don't trust
     def bad(self):
         pass
-        #deny ip src 192.168.42.0/24 dst any
- 
-        #if network == self.rules[0][3]
-        #src_ip = #need to get src IP from packet
-
-        #boolean = self.networkMatch(srcIP, network)
-        
-
 
     def networkMatch(self, ip, network):
         netMask = cidr_to_netmask(network.toUnsigned()) #should create netMask
@@ -127,12 +105,10 @@ class Firewall(object):
         else:
             return False
 
-
     def allow(self, pkt):
         print "ALLOW"
         print "packet:    ", pkt
-        #ip_header = pkt.find("ipv4")
-        
+
         self.import_rules()
 
         #for each rule, check the tcp or udp, src, dst, port,....
@@ -141,21 +117,11 @@ class Firewall(object):
         for i in range(0,len(self.rules)):
             ruleObject = self.rules[i]
             print ruleObject.line
-            #print packet.srcip
-            #print ruleObject.src
-            
-            #print "ruleObject.src:   ", ruleObject.src
-
-
-            #check type
-
             print pkt.payload
-
 
             if ruleObject.type != 1:
                 if (ruleObject.type == "x"):
                     print "HELL YA"
-            
 
             #check IP src
             if ruleObject.netMask != 1:
@@ -188,19 +154,16 @@ class Firewall(object):
                 print "pkt.payload.scrport:   ", pkt.payload.srcport
                 if (int(ruleObject.srcPort) == int(pkt.payload.srcport)):      #not sure why we have to typcast
                     print "OMFG"
-            
-            
-
-            
 
             #need to check if permit or deny
 
             #needs to be something like:
             #all(ruleObject.src == packet.src)(ruleObject.dst == packet.dst)(ruleObject.type = packet.type
-def tests():
-    print "YAY!!!!!!!!"
-    f = Firewall()
 
+
+#testing!!
+def tests():
+    f = Firewall()
     ip = pktlib.ipv4()
     ip.srcip = IPAddr("192.168.42.1")
     ip.dstip = IPAddr("172.16.42.42")
@@ -214,30 +177,11 @@ def tests():
 
     print len(ip) # print the length of the packet, just for fun
 
-    # you can name this method what ever you like, but you'll
-    # need some method that gets periodically invoked for updating
-    # token bucket state for any rules with rate limits
-    #f.update_token_buckets()
-
-    # again, you can name your "checker" as you want, but the
-    # idea here is that we call some method on the firewall to
-    # test whether a given packet should be permitted or denied.
     assert(f.allow(ip) == True)
 
-    # if you want to simulate a time delay and updating token buckets,
-    # you can just call time.sleep and then update the buckets.
     time.sleep(0.5)
     f.update_token_buckets()
 
 if __name__ == '__main__':
-    # only call tests() if this file gets invoked directly,
-    # not if it is imported.
     tests()
 
-
-
-
-#rule class -- match method (does rule match packet?)
-#   - also go based on token bucket?
-
-#class
